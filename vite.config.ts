70cf85c264044e60600ad97bbc1b8c77c6185443
lemur767 <lemur767trap@gmail.com>
2025-06-15 03:35:19 -0400

    Dep

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ command, mode }) => {
     build: {
       outDir: 'dist',
       sourcemap: false,
-      minify: 'terser',
+      minify: 'esbuild',
       rollupOptions: {
         output: {
           manualChunks: {
